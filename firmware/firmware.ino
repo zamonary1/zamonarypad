e6a75080b0435bb6cafc5fbf7fcf078541b766fb
Victor <victor.sochi.live@gmail.com>
2024-08-27 01:11:55 +0300

    BIG improvements to firmware, 200 times more efficient now, had to artificially lower the poll rate to 1000hz for buttons to function properly

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -24,30 +24,62 @@ USBHIDKeyboard Keyboard;
 #define analogRead
 
 
-//#define plotter
+//#define plotter //uncomment if you want to know poll rate
+                  //note: uncompatible with desktop app
 
 unsigned int value1;
 unsigned int value2;
 unsigned int button1_sensitivity;
 unsigned int button2_sensitivity;
 unsigned long millis_sleep_timer = 0;
-unsigned long millis_timeout_last_millis = 0;  //wtf
-//unsigned long timer_hz = millis();
 unsigned int i = 1;
 const unsigned short sleep_timeout = 10000;  //10 seconds
 bool touch1detected = false;
 bool touch2detected = false;
 
+bool button_1_pressed = false;
+bool button_2_pressed = false;
 
+
+#ifdef plotter
+  static long timer_a;
+  static int timer_b;
+#endif
+
+void press_button_1(){ //using these functions is keeping the load off USB
+  if (!button_1_pressed){    //less identical packets sent = less waiting for mcu = more poll rate
+  Keyboard.press('z');
+  button_1_pressed = true;
+  }
+}
+void release_button_1(){ //using these functions is keeping the load off USB
+  if (button_1_pressed){    //less identical packets sent = less waiting for mcu = more poll rate
+  Keyboard.release('z');
+  button_1_pressed = false;
+  }
+}
+
+void press_button_2(){ //using these functions is keeping the load off USB
+  if (!button_2_pressed){    //less identical packets sent = less waiting for mcu = more poll rate
+  Keyboard.press('x');
+  button_2_pressed = true;
+  }
+}
+void release_button_2(){ //using these functions is keeping the load off USB
+  if (button_2_pressed){    //less identical packets sent = less waiting for mcu = more poll rate
+  Keyboard.release('x');
+  button_2_pressed = false;
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  //delay(1000);  // give me some time to bring up serial monitor
+  delay(200);  // give me some time to bring up serial monitor
 
 
   Keyboard.begin();
   USB.begin();
-  pinMode(LED_BUILTIN, OUTPUT);
+  //pinMode(LED_BUILTIN, OUTPUT); board does not contain a built-in led
   EEPROM.begin(30);
 
 
@@ -121,51 +153,38 @@ void gotTouch2() {
 
 
 void loop() {
-#ifdef interrupts
-  if (touch1detected) {
-    if (touchInterruptGetLastStatus(btn_1_pin)) {
-      Keyboard.press('z');
-    } else {
-      Keyboard.release('z');
+  #ifdef interrupts
+    if (touch1detected) {
+      if (touchInterruptGetLastStatus(btn_1_pin)) {
+        Keyboard.press('z');
+      } else {
+        Keyboard.release('z');
+      }
+      touch1detected = false;
     }
-    touch1detected = false;
-  }
-  if (touch2detected) {
-    if (touchInterruptGetLastStatus(btn_2_pin)) {
-      Keyboard.press('x');
-    } else {
-      Keyboard.release('x');
+    if (touch2detected) {
+      if (touchInterruptGetLastStatus(btn_2_pin)) {
+        Keyboard.press('x');
+      } else {
+        Keyboard.release('x');
+      }
+      touch2detected = false;
     }
-    touch2detected = false;
-  }
-#endif
-#ifdef analogRead
+  #endif
+  #ifdef analogRead
 
-  if (touchRead(btn_1_pin) / 20 > button1_sensitivity) Keyboard.press('z');
-  else Keyboard.release('z');
+    if (touchRead(btn_1_pin) / 20 > button1_sensitivity) press_button_1();
+    else release_button_1();
 
-  if (touchRead(btn_2_pin) / 20 > button2_sensitivity) Keyboard.press('x');
-  else Keyboard.release('x');
+    if (touchRead(btn_2_pin) / 20 > button2_sensitivity) press_button_2();
+    else release_button_2();
 
-#endif
-  //  }
+  #endif
+
   if (Serial.available() > 0) {
 
     String inputString = Serial.readStringUntil('\n');  // read untill the next string
 
-    /*if (inputString == "readbtn1") {
-      // return the value of button 1
-      Serial.println(touchRead(btn_1_pin)/140);
-    } 
-    else if (inputString == "readbtn2") {
-      Serial.println(touchRead(btn_2_pin)/140);
-    } 
-    else if (inputString == "readbtn1sens"){
-      Serial.println(button1_sensitivity);
-    }
-    else if (inputString == "readbtn2sens"){
-      Serial.println(button2_sensitivity);
-    }*/
     if (inputString == "read") {
       StaticJsonDocument<100> response;
       response["button1val"] = touchRead(btn_1_pin) / 20.0;
@@ -192,10 +211,10 @@ void loop() {
       EEPROM.put(10, argument);
       EEPROM.commit();
       button1_sensitivity = argument;
-#ifdef interrupts
-      touchDetachInterrupt(btn_1_pin);
-      touchAttachInterrupt(btn_1_pin, gotTouch1, argument);
-#endif
+      #ifdef interrupts
+        touchDetachInterrupt(btn_1_pin);
+        touchAttachInterrupt(btn_1_pin, gotTouch1, argument);
+      #endif
       StaticJsonDocument<100> response;
       response["button1val"] = touchRead(btn_1_pin) / 20.0;
       response["button2val"] = touchRead(btn_2_pin) / 20.0;
@@ -210,10 +229,10 @@ void loop() {
       EEPROM.put(18, argument);
       EEPROM.commit();
       button2_sensitivity = argument;
-#ifdef interrupts
-      touchDetachInterrupt(btn_2_pin);
-      touchAttachInterrupt(btn_2_pin, gotTouch2, argument);
-#endif
+      #ifdef interrupts
+        touchDetachInterrupt(btn_2_pin);
+        touchAttachInterrupt(btn_2_pin, gotTouch2, argument);
+      #endif
       StaticJsonDocument<100> response;
       response["button1val"] = touchRead(btn_1_pin) / 20.0;
       response["button2val"] = touchRead(btn_2_pin) / 20.0;
@@ -239,6 +258,15 @@ void loop() {
       Serial.println(inputString);
     }
   }
+  #ifdef plotter
+    timer_b++;
+    if (timer_b==200){ //data rounding
+      Serial.println(String(1000.0 / (millis() - timer_a) * 200) + "hz rate");
+      timer_a = millis();
+      timer_b = 0;
+    }
+  #endif
+  delay(1);
 }
 
 
