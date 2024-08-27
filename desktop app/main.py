56801b195d7833bfad82a4f7a5d6bcc3f2d2ac8e
Victor <victor.sochi.live@gmail.com>
2024-08-28 00:45:03 +0300

    desktop app improvements, now uses gramms of force to use as sensitivity measure

--- desktop app/main.py
+++ desktop app/main.py
@@ -4,18 +4,19 @@ import serial
 import serial.tools.list_ports
 import time
 import json
+import scipy.interpolate
 
-# Глобальные переменные
+# global variables
 ser = None
 lasttime = time.time()
-json_response = {"button1val":0,"button2val":0,"button1sens":0,"button2sens":0}
+json_response = {"button1val":0,"button2val":0,"button1sens":0,"button2sens":0,"button1val_raw":0,"button2val_raw":0}
 json_response_raw = json_response
 min1 = float('inf')
 min2 = float('inf')
 max1 = float('-inf')
 max2 = float('-inf')
 delay_between_reads = 0.033 #33ms between updates or 30 updates per second
-# Функция для получения списка доступных последовательных портов
+# get availible serial interfaces
 def get_serial_ports():
     ports = serial.tools.list_ports.comports()
     ports = [port.device for port in ports]
@@ -32,7 +33,7 @@ def get_serial_ports():
         except SystemError: next
     return ports
 
-# Функция для установки последовательного соединения
+# create serial connection
 def connect_serial():
     global ser
     port = dpg.get_value("serial_port")
@@ -58,7 +59,6 @@ def lostConnection():
     dpg.set_value("progress_bar_2", 0.0)
     json_response = json.loads('{"button1val":0,"button2val":0}') #reset button statusbars to 0
 
-# Функция для отправки команды "readbtn1"
 def readbuttonvalues(raw = False):
     global lasttime
     global json_response
@@ -87,7 +87,8 @@ def readbuttonvalues(raw = False):
         max1 = max(max1, int(json_response['button1val']))
         max2 = max(max2, int(json_response['button2val']))
         
-        
+        json_response['button1val_raw'] = json_response['button1val']
+        json_response['button2val_raw'] = json_response['button2val']
         
         if max1 - min1 != 0:
             json_response['button1val'] = (int(json_response['button1val']) - min1) / (max1 - min1) * 100
@@ -107,19 +108,35 @@ def readbuttonvalues(raw = False):
     
     return(json_response)
 
+def send_sens_values(btn1_sens, btn2_sens):
+    if ser is not None:
+        json_response = readbuttonvalues()
+        val_btn1_idle = int(json_response["button1val_raw"])
+        val_btn2_idle = int(json_response["button2val_raw"])
+        print(f"sens1 = {val_btn1_idle + btn1_sens}, sens2 = {val_btn2_idle + btn2_sens}")
+        time.sleep(0.1)
+        ser.write(f'wrbtn1 {val_btn1_idle + btn1_sens}\n'.encode())
+        time.sleep(0.1)
+        ser.write(f'wrbtn2 {val_btn2_idle + btn2_sens}\n'.encode())
+        print(ser.readline())
+        return(True)
+    else: return(False)
+
 def calibrate_btn():
+    #Data was measured by me
+    x_grams =    [0.25, 0.45,  1.25,  1.6,   3.9,   9.5,   14,    29.6 ]
+    y_measured = [6000, 10000, 15000, 20000, 35000, 50000, 60000, 70000]
+
     if ser is not None:
         
         #pymsgbox.alert(text="Do not touch the zamonaryboard and press OK", title='', button='OK')
-        sensitivity = int(pymsgbox.prompt(text="Enter the sensitivity and do NOT touch the buttons", title='' , default='300'))
-        json_response_raw = readbuttonvalues(True)
-        val_btn1_idle = int(json_response_raw["button1val"])
-        val_btn2_idle = int(json_response_raw["button2val"])
-        print(json_response_raw, val_btn1_idle, val_btn2_idle)
-        time.sleep(0.1)
-        ser.write(f'wrbtn1 {val_btn1_idle + sensitivity}\n'.encode())
-        time.sleep(0.1)
-        ser.write(f'wrbtn2 {val_btn2_idle + sensitivity}\n'.encode())
+        sensitivity = int(pymsgbox.prompt(text="Enter the sensitivity (grams) and do NOT touch the buttons", title='' , default='1'))
+
+        cs = scipy.interpolate.CubicSpline(x_grams, y_measured, bc_type='natural')
+        
+        sensitivity = int(cs(sensitivity+0.3))
+
+        send_sens_values(sensitivity, sensitivity)
         pymsgbox.alert(text="Success", title='', button='OK')
 
 
@@ -139,30 +156,23 @@ with dpg.window(tag="mainwin"):
     serial_ports = get_serial_ports()
     print(serial_ports)
     serial_devices_list = dpg.add_combo(serial_ports, default_value=serial_ports[0] if serial_ports else "", tag="serial_port")
-    # if serial_ports == ["Not found"]:
-    #     dpg.hide_item(serial_devices_list)
-    # else: 
-    #     dpg.show_item(serial_devices_list)
 
-    # Кнопка для установки последовательного соединения
     dpg.add_button(callback=connect_serial, label='connect')
     dpg.add_button(callback=refresh_serial, label='refresh')
 
-    # Кнопки для отправки команд "readbtn1" и "readbtn2"
-    #dpg.add_button(callback=send_readbtn1, label='read 1')
-    #dpg.add_button(callback=send_readbtn2, label='read 2')
-
-    # Полоса загрузки для отображения полученных значений
     button_bar_1 = dpg.add_progress_bar(label="1", default_value=0, tag="progress_bar_1")
     button_bar_2 = dpg.add_progress_bar(label="2", default_value=0, tag="progress_bar_2")
     
     dpg.add_button(callback=calibrate_btn, label='calibrate')
 
-    # Метка для отображения статуса соединения
     dpg.add_text("", tag="connection_status")
+    dpg.add_text("", tag="values")
 
 dpg.show_viewport()
 dpg.set_primary_window("mainwin", True)
+
+
+
 
 while dpg.is_dearpygui_running():
     
@@ -172,6 +182,7 @@ while dpg.is_dearpygui_running():
     
     dpg.set_value("progress_bar_1", json_response['button1val']/100.0)
     dpg.set_value("progress_bar_2", json_response['button2val']/100.0)
+    dpg.set_value("values", (json_response['button1val_raw'], json_response['button2val_raw']))
     dpg.render_dearpygui_frame()
 dpg.destroy_context()
 
