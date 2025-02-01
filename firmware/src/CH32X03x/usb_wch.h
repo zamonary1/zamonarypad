52f0512f9302fb6510bfedb39fa3c0de9c18eb8c
Viktor <victor.sochi.live@gmail.com>
2025-02-01 22:33:22 +0300

    firmware as submodule

--- firmware/src/CH32X03x/usb_wch.h
+++
@@ -1,3 +0,0 @@
-void usb_init();
-void key_press(char key);
-void key_release(char key);
--- firmware/src/usb.h
+++
@@ -1,7 +0,0 @@
-#if defined(ESP32)
-    #include <esp32/usb_esp32.h>
-#elif defined(CH32X03)
-    #include <ch32x03x/usb_wch.h>
-#else
-    #error Used platform is not recognised! Report this error!
-#endif
